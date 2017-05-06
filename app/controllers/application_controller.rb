686f29046b2df4ff493f7bb9ce22d7bcd5b780cf
minhcvbachkhoa <minhcvbachkhoa@gmail.com>
2017-05-06 21:59:46 +0700

    finish search image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  before_filter :set_global_search_variable
+
   private
   def logged_in_user
     unless logged_in?
@@ -11,8 +13,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def search
+  def set_global_search_variable
     @q = Image.search params[:q]
-    @images = @q.result(distinct: true).order_by_created_at.page params[:page]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "/about", to: "static_pages#about"
+  post "/search", to: "static_pages#search"
   get "/sign_up", to: "users#new"
   get "/log_in", to: "sessions#new"
   post "/sign_up", to: "users#create"
   post "/log_in", to: "sessions#create"
   delete "/log_out", to: "sessions#destroy"
-
+  
   resources :users
-  resources :images
+  resources :images do
+    collection do
+      match "search" => "images#search", via: [:get, :post], as: :search
+    end
+  end
 end
